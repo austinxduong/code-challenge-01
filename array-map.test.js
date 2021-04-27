49e6491f16cae54b2234abae76d5d1bd44494142
austinxduong <axduong2@gmail.com>
2021-04-27 01:07:28 -0700

    set up 1st test

---
+++ array-map.test.js
@@ -0,0 +1,9 @@
+import { doubleNumbers } from './array-map';
+
+test('doubles numbers', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = doubleNumbers(input); // act
+  expect(output).toEqual([4, 10, 200]); // assert
+});
+
+// next test for capitalizeNames (add import too)
