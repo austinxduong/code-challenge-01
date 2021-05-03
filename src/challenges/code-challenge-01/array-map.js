e2c5fc73a3bbb45364848b1261a73e4e971ec616
austinxduong <axduong2@gmail.com>
2021-05-02 17:45:15 -0700

    passed namesOnly test

--- src/challenges/code-challenge-01/array-map.js
+++ src/challenges/code-challenge-01/array-map.js
@@ -10,3 +10,7 @@ export function capitalizeNames(arr) {
   const lowercased = arr.map(n => n.toLowerCase());
   return lowercased.map(n => n.charAt(0).toUpperCase() + n.substr(1).toLowerCase());
 }
+
+export function namesOnly(arr) {
+  return arr.map(n => n.name);
+}
