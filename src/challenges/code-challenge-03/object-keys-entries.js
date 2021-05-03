3e9e9cfaccfd5541531608bff371b83b63139cc8
austinxduong <axduong2@gmail.com>
2021-05-02 18:11:27 -0700

    passed sortedKeys test

--- src/challenges/code-challenge-03/object-keys-entries.js
+++ src/challenges/code-challenge-03/object-keys-entries.js
@@ -1,3 +1,9 @@
 export function capitalizeObjectKeys(obj) {
   return Object.keys(obj).map(key => key.toUpperCase());
 }
+
+export function sortedKeys(obj) {
+  return Object.keys(obj).sort((a, b) => {
+    return a.length - b.length;
+  });
+}
--- src/challenges/code-challenge-03/object-keys-entries.test.js
+++ src/challenges/code-challenge-03/object-keys-entries.test.js
@@ -1,7 +1,14 @@
-import { capitalizeObjectKeys } from './object-keys-entries';
+import { capitalizeObjectKeys, sortedKeys } from './object-keys-entries';
 
 test('it capitalizes the keys', () => {
   const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
   const output = capitalizeObjectKeys(dataObject); //act
   expect(output).toEqual(['NAME', 'AGE']); //assert
+});
+
+
+test('sorted keys', () => {
+  const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; //arrange
+  const output = sortedKeys(dataObject); //act
+  expect(output).toEqual(['age', 'name', 'isDog', 'friends']); //assert
 });
