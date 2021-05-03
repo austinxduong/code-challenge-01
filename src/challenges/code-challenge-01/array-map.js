f4425e8d0856a069a73931ec7c8fd5d29b350393
austinxduong <axduong2@gmail.com>
2021-05-02 17:37:29 -0700

    passed stringItUp test

--- src/challenges/code-challenge-01/array-map.js
+++ src/challenges/code-challenge-01/array-map.js
@@ -1,3 +1,7 @@
 export function doubleNumbers(arr) {
   return arr.map(x => x * 2);
 };
+
+export function stringItUp(arr) {
+  return arr.map(n => JSON.stringify(n));
+}
--- src/challenges/code-challenge-01/array-map.test.js
+++ src/challenges/code-challenge-01/array-map.test.js
@@ -1,8 +1,13 @@
-import { doubleNumbers } from './array-map.js';
+import { doubleNumbers, stringItUp } from './array-map.js';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
   const output = doubleNumbers(input); // act
   expect(output).toEqual([4, 10, 200]); // assert
 });
-// next test for capitalizeNames (add import too)
+
+test('string it up', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = stringItUp(input); // act
+  expect(output).toEqual(['2', '5', '100']); // assert
+});
