ba1b06485a407287839f7a6dfda9823148999e56
austinxduong <axduong2@gmail.com>
2021-05-10 00:05:32 -0700

    pass 1st tests

---
+++ src/challenges/code-challenge-07/more-object-keys-entries.test.js
@@ -0,0 +1,52 @@
+import { getHouses } from './more-object-keys-entries.js';
+
+const characters = [
+  {
+    name: 'Eddard', 
+    spouse: 'Catelyn', 
+    children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'], 
+    house: 'Stark'
+  },
+  {
+    name: 'Jon A.',
+    spouse: 'Lysa',
+    children: ['Robin'],
+    house: 'Arryn'
+  },
+  {
+    name: 'Cersei',
+    spouse: 'Robert',
+    children: ['Joffrey', 'Myrcella', 'Tommen'],
+    house: 'Lannister'
+  },
+  {
+    name: 'Daenarys',
+    spouse: 'Khal Drogo',
+    children: ['Drogon', 'Rhaegal', 'Viserion'],
+    house: 'Targaryen'
+  },
+  {
+    name: 'Mace',
+    spouse: 'Alerie',
+    children: ['Margaery', 'Loras'],
+    house: 'Tyrell'
+  },
+  {
+    name: 'Euron',
+    spouse: null,
+    children: [],
+    house: 'Greyjoy'
+  },
+  {
+    name: 'Jon S.',
+    spouse: null,
+    children: [],
+    house: 'Snow'
+  }
+];
+  
+test('it gets the names of the houses', () => {
+      //arrange
+  const output = getHouses(characters); //act
+  expect(output.sort()).toEqual(['Greyjoy', 'Snow', 'Arryn', 'Tyrell', 'Lannister', 'Targaryen', 'Stark'].sort()); //assert
+});
